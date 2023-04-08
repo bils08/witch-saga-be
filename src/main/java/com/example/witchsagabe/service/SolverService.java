66541c47a279c925de9d31ce9daf40e55bc94108
bils08 <billygj616.gmail.com>
2023-04-08 12:57:12 +0700

    add comment in service

--- src/main/java/com/example/witchsagabe/service/SolverService.java
+++ src/main/java/com/example/witchsagabe/service/SolverService.java
@@ -8,14 +8,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class SolverService {
 
+    //Method untuk mengecek validasi input user sebelum data dikelola
     public ResponseData findAverageKilledPerson(Person personA, Person personB) {
         ResponseData responseData = new ResponseData();
+        //cek jika input user bukan positive numbers
         if (personA.getAgeOfDeath() < 0 || personA.getYearOfDeath() < 0 || personB.getAgeOfDeath() < 0
                 || personB.getYearOfDeath() < 0) {
             responseData.setStatus(HttpStatus.BAD_REQUEST);
             responseData.setMessage("Person A and Person B Year of Death and Age of Death must be positive number!");
             responseData.setResult(-1);
             return responseData;
+         //cek jika input user age of death lebih besar dari year of death
         } else if (personA.getAgeOfDeath() > personA.getYearOfDeath() ||
                 personB.getAgeOfDeath() > personB.getYearOfDeath()) {
             responseData.setStatus(HttpStatus.BAD_REQUEST);
@@ -32,14 +35,17 @@ public class SolverService {
         }
     }
 
+    //Method untuk menghitung rata-rata person killed
     public float calculateAverageKilledPerson(int yearDeathA, int yearDeathB, int ageDeathA, int ageDeathB) {
         int yearA = yearDeathA - ageDeathA;
         int yearB = yearDeathB - ageDeathB;
-        float peopleKilledYearA = findPersonKilledByYear(yearA);
-        float peopleKilledYearB = findPersonKilledByYear(yearB);
+        float peopleKilledYearA = findPersonKilledByYear(yearA); //cari jumlah person killed tahun A
+        float peopleKilledYearB = findPersonKilledByYear(yearB); //cari jumlah person killed tahun B
         return (float) ((peopleKilledYearA + peopleKilledYearB) / 2);
     }
 
+
+    //Method untuk mencari jumlah person killed dalam n tahun
     public int findPersonKilledByYear(int year) {
         if (year <= 1) {
             return year;
